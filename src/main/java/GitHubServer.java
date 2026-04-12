2333a12ddc0d6974eb9ab9799b0d81b12c323a80
root <root@ip-172-31-31-162.eu-north-1.compute.internal>
2026-04-12 08:41:09 +0000

    Added pom.xml and java files

---
+++ src/main/java/GitHubServer.java
@@ -0,0 +1,23 @@
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.*;
+import javax.servlet.annotation.WebServlet;
+
+@WebServlet("/")
+public class GitHubServer extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        response.setContentType("text/html");
+
+        String html = "<html><body style='text-align:center;margin-top:100px;'>"
+                + "<h1>🚀 Welcome to My DevOps WAR Project</h1>"
+                + "<a href='https://github.com/navyasrigondela45'>GitHub</a><br><br>"
+                + "<a href='https://www.linkedin.com/in/navya-sri-gondela-a60091258'>LinkedIn</a>"
+                + "</body></html>";
+
+        response.getWriter().write(html);
+    }
+}
